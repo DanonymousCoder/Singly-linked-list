a562102d688b585d9d7bce04dd99c33449169061
Ridwan <aladebaruwa@gmail.com>
2023-08-08 22:00:43 +0100

    add

--- Linked-list.py
+++ Linked-list.py
@@ -4,5 +4,7 @@ class Node:
         self.next = None
     class singlylinked:
         def __init__(self) :
-            
+            self.head = None
+        def append (self, data)
+            new_node = Node(data)
     
